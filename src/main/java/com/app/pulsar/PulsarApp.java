f2c0eaa3d4ba0a05051d600bce53cae2e72cccdd
kcw7667 <Ke.Wu@YumChina.com>
2020-06-01 11:34:05 +0800

    优化部分配置与说明

--- src/main/java/com/app/pulsar/PulsarApp.java
+++ src/main/java/com/app/pulsar/PulsarApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PulsarApp {
-   public static void main(String[] args) {SpringApplication.run(PulsarApp.class, args);
-
-   }
+    public static void main(String[] args) {
+        SpringApplication.run(PulsarApp.class, args);
+    }
 }
--- src/main/java/com/app/pulsar/provGateway/PulsarClientImpl.java
+++ src/main/java/com/app/pulsar/provGateway/PulsarClientImpl.java
@@ -5,23 +5,26 @@ import org.apache.pulsar.client.api.PulsarClientException;
 
 public class PulsarClientImpl {
 
-	private static final String SERVICE_URL = "pulsar://192.168.10.10:6650";
-	private PulsarClientImpl() {
-	}
+    private static final String SERVICE_URL = "pulsar://bigdata1:6650,bigdata2:6650";
 
-	private static class PulsarClientObject {
-		private static final PulsarClient pulsarClient;
-		static {
-			try{
-				pulsarClient = PulsarClient.builder().serviceUrl(SERVICE_URL).build();
-			}catch(PulsarClientException e) {
-				throw new ExceptionInInitializerError(e);
-			}
-			
-		}
-	}
-	 public static PulsarClient getInstance(){
-	        return PulsarClientObject.pulsarClient;
-	    }
+    private PulsarClientImpl() {
+    }
+
+    private static class PulsarClientObject {
+        private static final PulsarClient pulsarClient;
+
+        static {
+            try {
+                pulsarClient = PulsarClient.builder().serviceUrl(SERVICE_URL).build();
+            } catch (PulsarClientException e) {
+                throw new ExceptionInInitializerError(e);
+            }
+
+        }
+    }
+
+    public static PulsarClient getInstance() {
+        return PulsarClientObject.pulsarClient;
+    }
 
 }
